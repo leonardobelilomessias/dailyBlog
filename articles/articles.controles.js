25ae9f1c4f41b520dff9eab2185c54feeac3a0d1
leonardobmessias <71788533+leonardobmessias@users.noreply.github.com>
2021-09-06 11:14:18 -0300

    alteração de tabelas

--- articles/articles.controles.js
+++ articles/articles.controles.js
@@ -1,14 +1,42 @@
 //Importação de recursos
 const express = require("express");
-const router = express.Router();
+const router = express.Router(); 
+const Category = require("../categories/Category");
+const Article = require("./Article");
+const slugify = require("slugify");
+
+
 
 //Definição de rotas
-router.get("/article",(req,res)=>{
-    res.send("Meus artigos! aqui")
+router.get("/admin/articles",(req,res)=>{
+    Article.findAll({
+        include:[{model:Category}]
+    }).then((articles)=>{
+        res.render("admin/articles/index",{articles,articles})
+    })
 });
 
-router.get("/article/new",(req,res)=>{
-    res.send("Novo artigo")
+router.get("/admin/articles/new",(req,res)=>{
+    Category.findAll().then(categories=>{
+        res.render("admin/articles/new",{categories:categories})
+    })
+    
 })
 
+
+router.post("/article/save",(req,res)=>{
+    let title = req.body.title;
+    let body = req.body.body;
+    let category = req.body.category
+
+    Article.create({
+        title:title,
+        slug:slugify(title),
+        body:body,
+        categoryId:category
+    }).then(()=>{
+        res.redirect("/admin/articles")
+    })
+});
+   
 module.exports = router;
--- categories/categories.controle.js
+++ categories/categories.controle.js
@@ -11,23 +11,62 @@ router.get("/admin/categories/new",(req,res)=>{
 
 router.post("/categories/save",(req,res)=>{
     let title = req.body.title;
-    if(title != undefined){
+    if(title != undefined && title !=null && title.trim().split() !="" ){
         Category.create({
             title:title,
             slug:slugify(title)
-        }).then(() => {res.redirect("/admin/categories/new")})
+        }).then(() => {res.redirect("/admin/categories")})
     }else{
         res.redirect("/admin/categories/new")
     }
-
 });
 
-router.get("/admin/categories/index",(req,res)=>{
+router.get("/admin/categories",(req,res)=>{
     Category.findAll().then(categories=>{
         res.render("admin/categories/index",{categories:categories})
-    })
-    
+    });  
+});
 
+router.post("/categories/delete",(req,res)=>{
+    let id = req.body.id;
+    if(id != undefined){
+        if(!isNaN(id)){
+            Category.destroy({where: {
+                id:id
+            }}).then(()=>{
+                res.redirect("/admin/categories");
+            })
+        }else{
+            res.redirect("/admin/categories");
+            
+        }
+    }else{
+        res.redirect("/admin/categories")
+    }
 })
+router.get("/admin/categories/edit/:id",(req,res)=>{
+    let id = req.params.id
+    if(isNaN(id)){
+        res.redirect("/admin/categories")
+    }
+    Category.findByPk(id).then(category=>{
+        if(category != undefined){
+            
+            res.render("admin/categories/edit",{category:category})
+        }else{
+            res.redirect("/admin/categories")
+        }
+    })
+})
+router.post("/categories/update",(req,res)=>{
+    let id = req.body.id
+    let title = req.body.title
 
-module.exports = router;
+    Category.update({title:title,slug:slugify(title)},{where:{
+        id:id
+    }}).then(()=>{
+        res.redirect("admin/categories")
+    })
+})
+ 
+module.exports = router; 
