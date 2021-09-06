6a15814b39643660b5cac4bb7011c3161a198ffd
leonardobmessias <71788533+leonardobmessias@users.noreply.github.com>
2021-09-06 08:48:10 -0300

    Criação de rota article e insersão de tinymce

--- articles/articles.controles.js
+++ articles/articles.controles.js
@@ -1,14 +1,21 @@
 //Importação de recursos
 const express = require("express");
-const router = express.Router();
+const router = express.Router(); 
+const Category = require("../categories/Category")
+
+
 
 //Definição de rotas
 router.get("/article",(req,res)=>{
     res.send("Meus artigos! aqui")
 });
 
-router.get("/article/new",(req,res)=>{
-    res.send("Novo artigo")
+router.get("/admin/articles/new",(req,res)=>{
+    Category.findAll().then(categories=>{
+        res.render("admin/articles/new",{categories:categories})
+    })
+    
 })
 
+   
 module.exports = router;
