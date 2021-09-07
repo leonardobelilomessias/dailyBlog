f114f81fb3cc28bd8a8478fa9ea6e436cf3c2f27
leonardobmessias <71788533+leonardobmessias@users.noreply.github.com>
2021-09-07 02:46:38 -0300

    criação de sistema de autenticação

--- articles/articles.controles.js
+++ articles/articles.controles.js
@@ -4,11 +4,12 @@ const router = express.Router();
 const Category = require("../categories/Category");
 const Article = require("./Article");
 const slugify = require("slugify");
+const adminAuth= require("../midwares/adminAuth")
 
 
 
 //Definição de rotas
-router.get("/admin/articles",(req,res)=>{
+router.get("/admin/articles",adminAuth,(req,res)=>{
     Article.findAll({
         include:[{model:Category}]
     }).then((articles)=>{
@@ -17,14 +18,16 @@ router.get("/admin/articles",(req,res)=>{
 
 });
 
-router.get("/admin/articles/new",(req,res)=>{
+router.get("/admin/articles/new",adminAuth,(req,res)=>{
     Category.findAll().then(categories=>{
         res.render("admin/articles/new",{categories:categories})
     })
     
 })
 
-router.post("/article/save",(req,res)=>{
+
+
+router.post("/article/save",adminAuth, (req,res)=>{
     let title = req.body.title;
     let body = req.body.body;
     let category = req.body.category
@@ -39,7 +42,7 @@ router.post("/article/save",(req,res)=>{
     })
 });
 
-router.post("/articles/delete",(req,res)=>{
+router.post("/articles/delete",adminAuth, (req,res)=>{
     let id = req.body.id;
     if(id != undefined){
         if(!isNaN(id)){
@@ -58,13 +61,7 @@ router.post("/articles/delete",(req,res)=>{
 
 });
 
-router.get("/admin/articles/new",(req,res)=>{
-    Category.findAll().then(categories=>{
-        res.render("admin/articles/new",{categories:categories})
-    })
-    
 
-})
 
 
 router.post("/article/save",(req,res)=>{
@@ -81,5 +78,66 @@ router.post("/article/save",(req,res)=>{
         res.redirect("/admin/articles")
     })
 });
+router.get("/admin/articles/edit/:id", adminAuth ,(req,res)=>{
+    let id = req.params.id
+    Article.findByPk(id).then(article=>{
+        if(article != undefined){
+            Category.findAll().then(categories=>{
+                res.render("admin/articles/edit", {categories:categories, article:article})
+            })
+            
+        }else{  
+            res.redirect("/")
+        }
+    }).catch(err=>{
+        res.redirect("/")
+    })
+})
+
+router.post("/articles/uptade", adminAuth, (req,res)=>{
+    let id = req.body.id
+    let title = req.body.title
+    let body = req.body.body
+    let category = req.body.category
+    Article.update({title:title,body:body, categoryId:category,slug:slugify(title)},{
+        where:{
+            id:id
+        }
+    }).then(()=>{
+        res.redirect("/admin/articles")
+    }).catch(err=>{
+        res.redirect("/")
+    })
+})
+router.get("/articles/page/:num",(req,res)=>{
+    let page = req.params.num
+    let offset = 0
+    if(isNaN(page) || page == 1 ){
+        offset = 0
+    }else{
+        offset = parseInt(page)*2
+    }
+
+    Article.findAndCountAll({
+        limit:2,
+        offset:offset
+    }).then(articles=>{
+        let next;
+        if(offset+2 >= articles.count){
+            next = false
+        }else{
+            next = true
+        }
+        let result ={
+            articles: articles,
+            next:next,
+            page: parseInt(page)
+        }
+        Category.findAll().then(categories=>{
+            res.render("admin/articles/page",{result:result,categories:categories})
+        })
+        
+    })
+})
    
 module.exports = router;
--- index.js
+++ index.js
@@ -6,11 +6,16 @@ const connection = require("./database/database");
 const categoriesControle = require("./categories/categories.controle");
 const articlesControle = require("./articles/articles.controles");
 const Article = require("./articles/Article");
-const Category = require("./categories/Category")
+const Category = require("./categories/Category");
+const usersControle = require("./user/Users.controle.js");
+const session = require("express-session")
+
 
 //Definição de template engine
 app.set('view engine', 'ejs');
-
+app.use(session({
+    secret:"kakscbeq4141qw2c",cookie:{maxAge:3000000}
+}))
 //Utilização do bodyparser(coletar dados via corpo formulario)
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
@@ -28,9 +33,12 @@ connection.authenticate().then(()=>{
 // Chamada de rotas
 app.use("/",categoriesControle);
 app.use("/",articlesControle);
+app.use('/',usersControle)
+
 app.get("/",(req,res)=>{
     Article.findAll({
-        order:[['id', 'DESC']]
+        order:[['id', 'DESC']],
+        limit:8
     }).then((articles)=>{
         Category.findAll().then(categories =>{
             res.render("index",{articles:articles,categories:categories});
@@ -40,23 +48,7 @@ app.get("/",(req,res)=>{
     
 });
 
-app.get("/:slug",(req,res)=>{
-    let slug = req.params.slug
-    Article.findOne({
-        where:{slug:slug}
-    }).then(article =>{
-        if(article != undefined){
-            Category.findAll().then(categories =>{
-                res.render("article",{article:article,categories:categories});
-            })
-        }else{
-            res.redirect("/")
-        }
-    }).catch(err =>{
-        res.redirect("/")
-    })
 
-})
 
 app.get("/category/:slug",(req,res)=>{
     let slug = req.params.slug
@@ -77,6 +69,39 @@ app.get("/category/:slug",(req,res)=>{
     })
 })
 
+app.get("/session",(req,res)=>{
+    req.session.nome = 'Leonardo'
+    req.session.idade =30
+    req.session.profissao = 'programador'
+    res.send('sessão gerda')
+
+})
+app.get("/leitura",(req,res)=>{
+    res.json({
+        nome:req.session.nome,
+        idade:req.session.idade,
+        profissao: req.session.profissao
+    })
+
+})
+
+app.get("/:slug",(req,res)=>{
+    let slug = req.params.slug
+    Article.findOne({
+        where:{slug:slug}
+    }).then(article =>{
+        if(article != undefined){
+            Category.findAll().then(categories =>{
+                res.render("article",{article:article,categories:categories});
+            })
+        }else{
+            res.redirect(`/${slug}`)
+        }
+    }).catch(err =>{
+        res.redirect("/")
+    })
+
+})
 // Abertura servidor
 app.listen(8080,()=>{
     console.log("servidor funcionado.")
